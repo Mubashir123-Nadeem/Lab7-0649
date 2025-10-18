6d36c754e0172176fd9808cea78f8101bfeee265
Mubashir Nadeem <K250649@nu.edu.pk>
2025-10-18 23:50:09 +0500

    Update Lab7 Q2.c

--- Lab7 Q2.c
+++ Lab7 Q2.c
@@ -1,27 +1,19 @@
-#include<stdio.h>
-//Q2
-int main() {
-	float studentMarks[10];
-	printf("Start entering student marks:\n");
-	int i;
-	float sum = 0;
-	float avg;
-	for(i = 0; i < 10; i++){
-		printf("Enter the marks of student [%d]: ", i+1);
-		scanf("%f", &studentMarks[i]);
-		
-	}
-	for(i = 0; i < 10; i++){
-		sum = sum + studentMarks[i];
-	}
-	avg = sum / 10;
-	printf("The average marks of the 10 students are: %f", avg);
-	
-	
-	
-	
-	
-	
-	return 0;
-}
+#include <stdio.h>
 
+int main() {
+    float studentMarks[10];
+    int i;
+    float sum = 0, avg;
+
+    printf("Start entering student marks:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Enter the marks of student [%d]: ", i + 1);
+        scanf("%f", &studentMarks[i]);
+        sum += studentMarks[i];
+    }
+
+    avg = sum / 10;
+    printf("The average marks of the 10 students are: %.2f\n", avg);
+
+    return 0;
+}
